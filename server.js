e05a2a664e3ef3c9fbff208adf7992a18f3160f1
Keiji O <meekunn@gmail.com>
2023-06-28 05:33:07 -0700

    added all view actions and exit action

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ const menu = () => {
             type: 'list',
             name: 'menu',
             message: 'Please choose from the following options.',
-            choices: ['1) View all Departments', '2) View all Roles', '3) View all Employees', '4) Add a Department', '5) Add a Role', '6) Add an Employee', '7) Update ad Employee Role'],
+            choices: ['1) View all Departments', '2) View all Roles', '3) View all Employees', '4) Add a Department', '5) Add a Role', '6) Add an Employee', '7) Update ad Employee Role', '8) Exit'],
         }
     ])
     .then(({menu}) => {
         if (menu === '1) View all Departments') {
-            return op1ViewDepartment();
+            return op1ViewAllDepartment();
         } else if (menu === '2) View all Roles') {
             return op2ViewAllRole();
         } else if (menu === '3) View all Employees') {
@@ -45,6 +45,8 @@ const menu = () => {
             return op6AddEmployee();
         } else if (menu === '7) Update ad Employee Role') {
             return op7UpdateEmployeeRole()
+        } else if (menu === '8) Exit') {
+            return exit();
         } else {
             console.log('error');
             return intro();
@@ -54,14 +56,38 @@ const menu = () => {
 
 intro();
 
-const op1ViewDepartment = () => {
-    console.log('1');
-    return menu();
+const op1ViewAllDepartment = () => {
+    const sql = `SELECT * FROM department
+    ORDER BY id;`;
+    db.query(sql, (err, results) => {
+      if (err) {
+        console.log("Error: something went wrong");
+        return menu();
+      }  else{
+        console.log(`
+-----All Department-----`);
+        console.table(results);
+        return menu();
+      }
+    });
 };
 
 const op2ViewAllRole = () => {
-    console.log('2');
-    return menu();
+    const sql = `SELECT r.id, r.title, r.salary, department.name
+    FROM role r
+    JOIN department ON r.department_id = department.id
+    ORDER BY id;`;
+    db.query(sql, (err, results) => {
+      if (err) {
+        console.log("Error: something went wrong");
+        return menu();
+      }  else{
+        console.log(`
+-----All Role-----`);
+        console.table(results);
+        return menu();
+      }
+    });
 };
 
 const op3ViewAllEmployees = () => {
@@ -102,3 +128,12 @@ const op7UpdateEmployeeRole = () => {
     console.log('7');
     return menu();
 };
+
+const exit = () => {
+    console.clear();
+    console.log(`----------
+Exiting Employee Manager...
+
+Please input "ctrl + c" in terminal to close this app.
+----------`);
+}
